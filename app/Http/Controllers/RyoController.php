492ef8c2eacfd752ba95daa1715d72faa42dafa1
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-12-12 06:43:23 +0900

    Initial commit

---
+++ app/Http/Controllers/RyoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RyoController extends Controller
+{
+    public function ryo(Request $request){
+        //バリデーション
+        //dbへの入力処理
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sakon', function () {
+    return view('sakon');
+});
+
+Route::get('todo/create', 'Admin\TodoController@add');
+//todo/createはsakon.com以下のディレクトリを表し、アクセスされたページのこと。
+//Admin\TodoControllerはHTTPをrootとしたディレクトリの位置。@addはコントローラのAction名。
+//a
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => ''], function() {
+    Route::get('todo/create', 'Admin\TodoController@add')->middleware('auth');
+});
