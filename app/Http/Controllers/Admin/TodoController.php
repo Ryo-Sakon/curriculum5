65d73aeb30bcac8cc5bef4c9b337f4aa80b3abd6
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-12-12 17:45:44 +0900

    a

--- app/Http/Controllers/Admin/TodoController.php
+++ app/Http/Controllers/Admin/TodoController.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+// namespaceとは、PHPで名前空間（エイリアス）を設定するために使用されるものです。
+// 一般的にPHPフォルダーのはじめにnamespace <ファイルのパス>と記載します。
+// エイリアスをファイルのパスにすることで自動読み込みを行うことができるようにしています。
+
+namespace App\Http\Controllers\Admin;//ファイルの位置
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -13,9 +17,12 @@ class TodoController extends Controller
         //resourse/views/todo/create.blade.php
     }
 
-    public function create(Request $request)
+    public function create(Request $request) //Requestクラスの変数$request
     {
       // todo/createにリダイレクトする
         return redirect('todo/create');
     }
 }
+//このメソッドの実行は、どこに書かれているか？→web.php
+//ここは、機能そのもの
+//createメソッドを実行するという発想はどうやる？
--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,7 @@
 Route::get('', function () {
     return view('welcome');
 });
+//ルール
 
-Route::get('sakon', function () {
-    return view('sakon');
-});
-
-Route::get('todo/ryo', function () {
-    return view('todo/ryo');
-});
-
-Route::get('todo/create', 'Admin\TodoController@add');
-//todo/createはsakon.com以下のディレクトリを表し、アクセスされたページのこと。
-//app/HTTP/Admin/TodoController内に書かれたadd関数を実行
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['prefix' => ''], function() {
-    Route::get('todo/create', 'Admin\TodoController@add')->middleware('auth');
-    Route::post('todo/create', 'Admin\TodoController@create');
-});
 //解説欲しい。Routeはどこにある？get,routesとは？middlewareは？groupは？
+//b.php
